a8679e621b45f7a2a9df591f6c19db6bcba00452
Preetam <vpulla@asu.edu>
2026-05-15 12:16:04 -0700

    Added CORS Origin for all the backend to interact with frontend

--- src/main/java/com/example/travel/ai/AiController.java
+++ src/main/java/com/example/travel/ai/AiController.java
@@ -3,12 +3,10 @@ package com.example.travel.ai;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/ai")
 public class AiController {
 
--- src/main/java/com/example/travel/booking/BookingController.java
+++ src/main/java/com/example/travel/booking/BookingController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/bookings")
 public class BookingController {
     private final BookingService bookingService;
